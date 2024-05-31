3577290e11d789f36ea2e0b3c9edd70fd91e56b2
Gil <gilpasi@gmail.com>
2024-05-31 12:38:32 +0300

    All simple routing converted to DRF in API views

--- backend/mappify-server/MappifyApp/api_urls.py
+++ backend/mappify-server/MappifyApp/api_urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .api_views import example_request, get_csrf_token
 from .api_views import UploadVideoAPIView
 
 
 urlpatterns = [
-    path('', example_request, name='example_of_how_to_make_request'),
-    path('get-csrf-token/', get_csrf_token, name='example_of_how_to_make_request'),
+    path('', UploadVideoAPIView.as_view(), name='example_of_how_to_make_request'),
+    path('get-csrf-token/', UploadVideoAPIView.as_view(), name='example_of_how_to_make_request'),
+    # path('', example_request, name='example_of_how_to_make_request'),
+    # path('get-csrf-token/', get_csrf_token, name='example_of_how_to_make_request'),
     path('upload/', UploadVideoAPIView.as_view(), name='upload_video'),
+
 ]
--- backend/mappify-server/MappifyApp/api_views.py
+++ backend/mappify-server/MappifyApp/api_views.py
@@ -6,53 +6,73 @@ from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from django.http import HttpResponse
 import os 
-
-
-
-# class VideoListCreate(generics.ListCreateAPIView):
-#     queryset = Video.objects.all()
-#     serializer_class = VideoSerializer
-    
-
-# class VideoDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Video.objects.all()
-#     serializer_class = VideoSerializer
-
-
-# class UploadVideo(generics.CreateAPIView):
-#     queryset = Video.objects.all()
-#     serializer_class = VideoSerializer
-
-#     # parser_classes = (MultiPartParser, FormParser)
-
-#     def post(self, request, *args, **kwargs):
-#         try:
-#             print('request is: ', request)
-#             print('POST is: ', request.POST)
-#             print('FILES is: ', request.FILES)
-
-#             serializer = self.get_serializer(data=request.data)
-#             print("2")
-#             if serializer.is_valid():
-#                 print("3")
-
-#                 serializer.save()
-#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-#             else:
-#                 print("4")
-#                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         except Exception as err: 
-#             print("error:", err)
-# views.py
-
-import os
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import VideoUploadSerializer
 
 class UploadVideoAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        if self.request.path.endswith('get-csrf-token/'):
+            return self.get_csrf_token(request)
+        elif self.request.path.endswith('/'):
+            return self.example_request(request)
+        else:
+            return Response({'error': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def get_csrf_token(self, request):
+        csrf_token = get_token(request)
+        print(f"CSRF Token: {csrf_token}")
+        return Response({'csrfToken': csrf_token})
+
+    def example_request(self, request):
+        print("AAAAA")
+        query_post_example = """
+        let formData = new FormData();
+        formData.append('video', {
+          uri,
+          name: `video.${fileType}`,
+          type: `video/${fileType}`
+        });
+
+         url = `${getBaseUrl()}/api/upload/`
+        const response = await fetch(url, {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'X-CSRFToken': csrfToken
+          },
+         })
+        ===================================
+        If case of 403 error consider getting
+        a csrf cookie prior to the request
+        in the path /api/get_csrf_token
+        use it like so:
+        ===================================
+
+
+          const fetchCsrfToken = async () => {
+        try {
+          url = `${getBaseUrl()}/api/get-csrf-token/`
+          const response = await fetch(url);
+          const token = extractCsrfToken(response);
+          csrfRef.current = token
+          console.log("CSRF Token:", token);
+        } catch (err) {
+          console.error("Something went wrong while querying CSRF token:", err);
+        }
+      };"""
+        
+        return HttpResponse(query_post_example, content_type="text/plain")
+
+
     def post(self, request, *args, **kwargs):
+        if self.request.path.endswith('upload/'):
+            return self.upload_video(request)
+        else:
+            return Response({'error': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def upload_video(self, request, *args, **kwargs):
         serializer = VideoUploadSerializer(data=request.data)
         if serializer.is_valid():
             video = serializer.validated_data['video']
@@ -66,48 +86,3 @@ class UploadVideoAPIView(APIView):
             
             return Response({'message': 'Video uploaded successfully!'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-def example_request(request):
-    query_post_example = """
-    let formData = new FormData();
-    formData.append('video', {
-      uri,
-      name: `video.${fileType}`,
-      type: `video/${fileType}`
-    });
-
-     url = `${getBaseUrl()}/api/upload/`
-    const response = await fetch(url, {
-      method: 'POST',
-      body: formData,
-      headers: {
-        'X-CSRFToken': csrfToken
-      },
-     })
-    ===================================
-    If case of 403 error consider getting
-    a csrf cookie prior to the request
-    in the path /api/get_csrf_token
-    use it like so:
-    ===================================
-
-
-      const fetchCsrfToken = async () => {
-    try {
-      url = `${getBaseUrl()}/api/get-csrf-token/`
-      const response = await fetch(url);
-      const token = extractCsrfToken(response);
-      csrfRef.current = token
-      console.log("CSRF Token:", token);
-    } catch (err) {
-      console.error("Something went wrong while querying CSRF token:", err);
-    }
-  };"""
-    
-    return HttpResponse(query_post_example, content_type="text/plain")
-
-
-def get_csrf_token(request):
-    csrf_token = get_token(request)
-    print(f"CSRF Token: {csrf_token}")
-    return JsonResponse({'csrfToken': csrf_token})
