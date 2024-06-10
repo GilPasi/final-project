10d7db890f3443d307800a4eaf88ce01387f7d69
Kevin Sbarski <kevinsbarski@gmail.com>
2024-06-10 13:48:29 +0300

    .venv clean up

---
+++ frontend/mappifiy-client/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Page from './Components/Page';
+import {View, Text} from  'react-native'
+import AccelerometerScreen from './Components/AccelerometerScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <View>
+      <Page/>
+      <AccelerometerScreen/>
+    </View>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Home" component={Page} />
+    //     <Stack.Screen name="Details" component={Accelerometer} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+  );
+}
+
+export default App;
